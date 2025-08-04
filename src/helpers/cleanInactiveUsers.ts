f23d3b52605da39a27da5acaf99fbdb85cb4de3b
rakiburrahman307 <rakiburrahman307@gmail.com>
2025-08-04 16:00:19 +0600

    feat(users): add cleanup function for inactive users
    Implement a function to automatically mark users as offline if they haven't sent a heartbeat in 5 minutes. This helps maintain accurate online statuses in the system.

---
+++ src/helpers/cleanInactiveUsers.ts
@@ -0,0 +1,27 @@
+import { User } from "../app/modules/user/user.model";
+
+export const cleanupInactiveUsers = async () => {
+  const fiveMinutesAgo = new Date(Date.now() - 5 * 60 * 1000);
+  
+  try {
+    const inactiveUsers = await User.find({
+      'onlineStatus.isOnline': true,
+      'onlineStatus.lastHeartbeat': { $lt: fiveMinutesAgo }
+    });
+
+    for (const user of inactiveUsers) {
+      await User.setUserOffline(user._id.toString());
+      
+      // If you have access to io instance, broadcast offline status
+      // io.emit('user_offline', {
+      //   userId: user._id.toString(),
+      //   isOnline: false,
+      //   lastSeen: new Date(),
+      // });
+    }
+
+    console.log(`Cleaned up ${inactiveUsers.length} inactive users`);
+  } catch (error) {
+    console.error('Cleanup job failed:', error);
+  }
+};
