98d09c44ea8408e8168f3b4e39d6a1fa3f8ccaf5
rakiburrahman307 <rakiburrahman307@gmail.com>
2025-08-07 14:45:15 +0600

    fix(auth): add check for deleted user status
    Prevent deleted/banned users from accessing protected routes by verifying user status before authorization

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -7,38 +7,42 @@ import { jwtHelper } from '../../helpers/jwtHelper';
 
 const auth =
   (...roles: string[]) =>
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const tokenWithBearer = req.headers.authorization;
+    async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        const tokenWithBearer = req.headers.authorization;
 
-      if (!tokenWithBearer) {
-        throw new ApiError(StatusCodes.UNAUTHORIZED, 'You are not authorized');
-      }
-
-      if (tokenWithBearer && tokenWithBearer.startsWith('Bearer')) {
-        const token = tokenWithBearer.split(' ')[1];
-
-        //verify token
-        const verifyUser = jwtHelper.verifyToken(
-          token,
-          config.jwt.jwt_access_token_secret as Secret
-        );
-        //set user to header
-        req.user = verifyUser;
-
-        //guard user
-        if (roles.length && !roles.includes(verifyUser.role)) {
-          throw new ApiError(
-            StatusCodes.FORBIDDEN,
-            "You don't have permission to access this api"
-          );
+        if (!tokenWithBearer) {
+          throw new ApiError(StatusCodes.UNAUTHORIZED, 'You are not authorized');
         }
 
-        next();
+        if (tokenWithBearer && tokenWithBearer.startsWith('Bearer')) {
+          const token = tokenWithBearer.split(' ')[1];
+
+          //verify token
+          const verifyUser = jwtHelper.verifyToken(
+            token,
+            config.jwt.jwt_access_token_secret as Secret
+          );
+          //set user to header
+          req.user = verifyUser;
+
+          if (verifyUser?.status === 'delete') {
+            throw new ApiError(StatusCodes.FORBIDDEN, 'This user is banned !!');
+
+          }
+          //guard user
+          if (roles.length && !roles.includes(verifyUser.role)) {
+            throw new ApiError(
+              StatusCodes.FORBIDDEN,
+              "You don't have permission to access this api"
+            );
+          }
+
+          next();
+        }
+      } catch (error) {
+        next(error);
       }
-    } catch (error) {
-      next(error);
-    }
-  };
+    };
 
 export default auth;
